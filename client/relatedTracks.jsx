06992d0e2d17a6316a0f5f27af81e536456dc74f
Nicholas Tolle <nbtolle@gmail.com>
2020-09-22 20:47:26 -0600

    Work on client side

--- client/relatedTracks.jsx
+++ client/relatedTracks.jsx
@@ -5,19 +5,20 @@ class relatedTracks extends React.Component {
     super(props);
     this.state = {
       currentSong: 1,
-      plays: 0,
-      likes: 0,
-      reposts: 0,
-      img: '',
-      band: '',
-      song: ''
+      related: []
     }
   }
 
   render() {
     return (
       <>
-
+        <img className="band" src="https://i1.sndcdn.com/avatars-000153260008-nj3jj1-t200x200.jpg"/>
+        <p>LionsBesideUs</p>
+        <p>Last Chance</p>
+        <p>&#9658 4</p>
+        <p>&#9829 6</p>
+        <p>&#10226 2</p>
+        <p>&#128488 3</p>
       </>
     )
   }
