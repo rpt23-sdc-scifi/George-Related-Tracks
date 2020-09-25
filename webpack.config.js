dca2d45190e737c51e8ab6569e56143493bd28be
Nicholas Tolle <nbtolle@gmail.com>
2020-09-24 21:40:27 -0600

    Continue working on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,20 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
 };
