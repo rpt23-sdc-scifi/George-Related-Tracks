eb8a7f73363034e09c5e147d310557e8f0e5eddf
Nicholas Tolle <nbtolle@gmail.com>
2020-09-26 19:36:38 -0600

    Fix symbols so they display correctly on page

--- client/index.jsx
+++ client/index.jsx
@@ -11,15 +11,19 @@ class RelatedTracks extends React.Component {
   }
 
   render() {
+    let plays = [<span>&#9658;</span>, ` 4`];
+    let likes = [<span>&#9829;</span>, ` 6`];
+    let reposts = [<span>&#10226;</span>, ` 2`];
+    let comments = [<span>&#128488;</span>, ` 3`];
     return (
       <>
         <img className="nicholas related-tracks band" src="https://i1.sndcdn.com/avatars-000153260008-nj3jj1-t200x200.jpg"/>
         <p>LionsBesideUs</p>
         <p>Last Chance</p>
-        <p>&#9658 4</p>
-        <p>&#9829 6</p>
-        <p>&#10226 2</p>
-        <p>&#128488 3</p>
+        <p>{plays}</p>
+        <p>{likes}</p>
+        <p>{reposts}</p>
+        <p>{comments}</p>
       </>
     )
   }
