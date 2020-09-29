3c0108022167e08622bb120f9395314fac18662e
Nicholas Tolle <nbtolle@gmail.com>
2020-09-28 18:55:39 -0600

    Get api call working

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 class RelatedTracks extends React.Component {
   constructor(props) {
@@ -8,22 +9,39 @@ class RelatedTracks extends React.Component {
       currentSong: 1,
       related: []
     }
+
+    this.updateRelated = this.updateRelated.bind(this);
+  }
+
+  updateRelated(data) {
+    this.setState({related: data});
+  }
+
+  componentDidMount() {
+    $.ajax(
+      {
+        url: '/relatedTracks/1',
+        data: {},
+        success: data => this.updateRelated(data),
+        error: err => console.log(err)
+      }
+    );
   }
 
   render() {
-    let plays = [<span>&#9658;</span>, ` 4`];
-    let likes = [<span>&#9829;</span>, ` 6`];
-    let reposts = [<span>&#10226;</span>, ` 2`];
-    let comments = [<span>&#128488;</span>, ` 3`];
+    let plays = [<span key="spanPlays">&#9658;</span>, ` 4`];
+    let likes = [<span key="spanLikes">&#9829;</span>, ` 6`];
+    let reposts = [<span key="spanReposts">&#10226;</span>, ` 2`];
+    let comments = [<span key="spanComments">&#128488;</span>, ` 3`];
     return (
       <>
-        <img className="nicholas related-tracks band" src="https://i1.sndcdn.com/avatars-000153260008-nj3jj1-t200x200.jpg"/>
-        <p>LionsBesideUs</p>
-        <p>Last Chance</p>
-        <p>{plays}</p>
-        <p>{likes}</p>
-        <p>{reposts}</p>
-        <p>{comments}</p>
+        <img key="image" className="nicholas related-tracks band" src="https://i1.sndcdn.com/avatars-000153260008-nj3jj1-t200x200.jpg"/>
+        <p key="band">LionsBesideUs</p>
+        <p key="song">Last Chance</p>
+        <p key="plays">{plays}</p>
+        <p key="likes">{likes}</p>
+        <p key="reposts">{reposts}</p>
+        <p key="comments">{comments}</p>
       </>
     )
   }
