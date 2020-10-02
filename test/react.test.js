76049ab78175f224ae2d6e1dd1c231e60497307a
Nicholas Tolle <nbtolle@gmail.com>
2020-10-01 21:37:54 -0600

    Attempt database test

--- test/react.test.js
+++ test/react.test.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import RelatedTracks from '../client';
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+// import RelatedTracks from '../client';
+const {MongoClient} = require('mongodb');
 
-test('Page renders', () => {
-  const component = renderer.create(
-    <RelatedTracks></RelatedTracks>
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+// test('Page renders', () => {
+//   const component = renderer.create(
+//     <RelatedTracks></RelatedTracks>
+//   );
+//   let tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
+
+describe('insert', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(global.test, {
+      useNewUrlParser: true,
+    });
+    db = await connection.db(global.Track);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
+
+  it('should insert a doc into collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = {_id: 'some-user-id', name: 'John'};
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(mockUser);
+  });
 });
