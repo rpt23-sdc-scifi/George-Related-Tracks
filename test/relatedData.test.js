f8b782062715e58f5c2ffb7faf71cc12186c27c8
Nicholas Tolle <nbtolle@gmail.com>
2020-10-02 16:31:52 -0600

    Add test and add jest.config.js back into repo

--- test/relatedData.test.js
+++ test/relatedData.test.js
@@ -37,3 +37,30 @@ describe('insert', () => {
     expect(insertedUser).toEqual(mockUser);
   });
 });
+
+describe('delete', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect('mongodb://localhost/test', {
+      useNewUrlParser: true,
+    });
+    db = await connection.db('Track');
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
+
+  it('should insert a doc into collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = {_id: 'some-user-id', name: 'John'};
+    await users.findOneAndDelete(mockUser);
+
+    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(null);
+  });
+});
