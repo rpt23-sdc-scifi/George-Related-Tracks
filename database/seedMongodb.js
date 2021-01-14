be38c8e4f2055e13ab226752344806ab28c8c861
George Humphrey <george.p.humphrey@gmail.com>
2021-01-14 13:28:59 -0500

    database deployment debugging

--- database/seedMongodb.js
+++ database/seedMongodb.js
@@ -1,14 +1,17 @@
+console.log('file opened')
 const chalk = require('chalk');
 const { dirname } = require('path');
 const streamToMongoDB = require('stream-to-mongo-db').streamToMongoDB;
 const JSONStream = require('JSONStream');
 const fs = require('fs');
 const Multistream = require('multistream');
+console.log('defined dependencies')
 
 const relatedData = require('./relatedData.js');
 const json1 = require(`${__dirname}/data/records1.json`);
 const json2 = require(`${__dirname}/data/records2.json`);
 const json3 = require(`${__dirname}/data/records3.json`);
+console.log('defined internal dependencies')
 
 const outputDBConfig = {
   dbURL: 'mongodb://localhost:27017/relatedTracks',
@@ -21,6 +24,8 @@ const streams = [
   fs.createReadStream(__dirname + '/data/records2.json'),
   fs.createReadStream(__dirname + '/data/records3.json')
 ]
+
+console.log('created streams')
 
 function seedMongo() {
   console.log('called seedMongo; attempting db drop')
@@ -38,5 +43,6 @@ function seedMongo() {
       })
   })
 }
+console.log('defined seeding function')
 
 seedMongo();
